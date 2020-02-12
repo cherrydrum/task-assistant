0f78b2dac62d73ba98c271e51099f00b0573e70d
Qu1s <trof1m00@mail.ru>
2020-02-12 22:15:06 +0300

    Flask test

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from models import *
+import os
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath('task-assistant.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# login_manager = LoginManager(app)
+db = SQLAlchemy(app)
