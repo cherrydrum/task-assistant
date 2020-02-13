c4af505cb54980327eda63b4ae97dd76e0a41ef4
Qu1s <trof1m00@mail.ru>
2020-02-13 18:43:04 +0300

    Базы данных, логин, профили

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from app import *
-from views import index
+from views import index, login, register, logout, profile, p404
 
 
 if __name__ == "__main__":
+    app.secret_key = '123'
     app.run(debug=True)
--- app/setup.py
+++ app/setup.py
@@ -1,1 +1,7 @@
 from app import *
+from models import users, tasks
+
+
+db.drop_all()
+db.create_all()
+db.session.commit()
